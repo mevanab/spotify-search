a6d9dde11339079393eec7be58303f9006e9a8b5
Mevan Abeydeera <mevan.villvay@gmail.com>
2017-05-12 09:09:16 +0530

    Fix ClassName & Remove Icon Style

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -2,11 +2,8 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
     render() {
-        const iconStyles = {
-        };
-
         return(
-            <footer class="App-footer">
+            <footer className="App-footer">
                 Crafted by Mevan Abeydeera.
             </footer>
         );
